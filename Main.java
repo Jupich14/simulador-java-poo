81e3ddeaf5026ae1f81ca3a748cd593a1954414c
Joel <c.joelperez@outlook.com>
2026-09-16 08:04:42 -0600

    Agregar variables en Java y crear .gitignore

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void  main(String[]args){
+        String nombre = "Joel";
+        int nivel = 1;
+        System.out.println("Hola, Soy "+ nombre + " y estoy en el nivel "+ nivel + " de Java" );
+    }
+}
