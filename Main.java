71d6bb3e6af5afc7413f82d3a0ff741d1ba9098d
Joel <c.joelperez@outlook.com>
2026-09-16 19:39:25 -0600

    Agregar condicionales if/else para validar el nivel

--- Main.java
+++ Main.java
@@ -3,5 +3,12 @@ public class Main {
         String nombre = "Joel";
         int nivel = 1;
         System.out.println("Hola, Soy "+ nombre + " y estoy en el nivel "+ nivel + " de Java" );
+
+        //cCondicionales 
+        if (nivel==1){
+            System.out.println("Soy un estudiante, que aprende rapido");
+        }else{
+            System.out.println("Ya tango experienia en esto");
+        }
     }
 }
